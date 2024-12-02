9049349786f099bcd709e0f056e467d6d8bfb609
mcquenji <benjamin.mceachnie@necode.it>
2024-12-02 03:30:46 +0000

    fix: include environment variables in compilation step

--- start.sh
+++ start.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 set -e
 
+printenv >.env
+
 echo "Compiling application..."
 
-fvm flutter build web --release -o /usr/share/nginx/html
+fvm flutter build web --release -o /usr/share/nginx/html --dart-define-from-file=.env
 
 echo "🚀 Done! Application is running at http://localhost:80"
 
