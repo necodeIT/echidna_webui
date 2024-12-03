5c0dcf448328ab3fae963a7a86df93e4d2297d9d
mcquenji <benjamin.mceachnie@necode.it>
2024-12-03 02:19:42 +0000

    feat: update build script to include timestamp in version.json for cache invalidation

--- start.sh
+++ start.sh
@@ -3,9 +3,10 @@ set -e
 
 printenv >.env
 
-echo "Compiling application..."
-
 fvm flutter build web --release -o /usr/share/nginx/html --dart-define-from-file=.env
+
+# replace "build_nummer":"1" with milliseconds since epoch in /usr/share/nginx/html/version.json
+sed -i "s/build_number\":\s*\"[0-9]*\"/build_number\": \"$(date +%s)\"/g" /usr/share/nginx/html/version.json
 
 echo "🚀 Done! Application is running at http://localhost:80"
 
